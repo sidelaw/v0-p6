43835b665af22b441ee5eaae5bbdf86aead1f06e
iabdullahbinnasir <abdullahnasir2000@gmail.com>
2025-10-17 18:00:53 +0500

    minot changes to themeprovider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,12 +1,14 @@
-'use client';
+'use client'
 
-import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import * as React from 'react'
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps as BaseProps,
+} from 'next-themes'
 
-// Make sure the props include `children`
-export type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
-// or: export type ThemeProviderProps = Parameters<typeof NextThemesProvider>[0];
+// ✅ Add `children` to the exported props
+type Props = BaseProps & { children?: React.ReactNode }
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+export function ThemeProvider({ children, ...props }: Props) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
